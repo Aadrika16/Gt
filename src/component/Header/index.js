a06f030e135e79062abf21688aa3fde16af2df98
Aadrika16 <nemurimadhuri@gmail.com>
2026-04-29 09:28:29 +0530

    Updated UI or feature

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,72 +1,66 @@
-
-import { Link, useLocation } from 'react-router-dom'
+import { useState } from "react"
+import { Link, useLocation } from "react-router-dom"
 import {
   FaHome,
   FaServicestack,
   FaInfoCircle,
-  FaUsers
-} from 'react-icons/fa'
+  FaUsers,
+  FaBars,
+  FaTimes
+} from "react-icons/fa"
 
-import './index.css'
+import "./index.css"
 
 const Header = () => {
   const location = useLocation()
+  const [open, setOpen] = useState(false)
 
   return (
-    <nav className="navbar">
+    <header className="header">
+      {/* Logo */}
       <div className="logo">
         <img
           src="https://res.cloudinary.com/doyaebals/image/upload/v1777117610/82023ee6966f15c767c25263f523a655a0945cfe_bxiaan.png"
           alt="logo"
-          className="logo-icon"
         />
       </div>
-        <ul className="nav-links">
-          <li>
-            <Link
-              to="/"
-              className={location.pathname === '/' ? 'active-link' : ''}
-            >
-              <FaHome className="nav-icon" />
-              <span className="nav-text">Home</span>
-            </Link>
-          </li>
 
-          <li>
-            <Link
-              to="/services"
-              className={location.pathname === '/services' ? 'active-link' : ''}
-            >
-              <FaServicestack className="nav-icon" />
-              <span className="nav-text">Services</span>
-            </Link>
-          </li>
+      {/* Hamburger */}
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        {open ? <FaTimes /> : <FaBars />}
+      </div>
 
-          <li>
-            <Link
-              to="/about"
-              className={location.pathname === '/about' ? 'active-link' : ''}
-            >
-              <FaInfoCircle className="nav-icon" />
-              <span className="nav-text">About</span>
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              to="/clients"
-              className={location.pathname === '/clients' ? 'active-link' : ''}
-            >
-              <FaUsers className="nav-icon" />
-              <span className="nav-text">Clients</span>
-            </Link>
-          </li>
-        </ul>
-          <Link to="/contact" className="nav-btn">
-              <span className='contact-text'>BOOK NOW</span> 
+      {/* Nav */}
+      <ul className={`nav-menu ${open ? "active" : ""}`}>
+        <li>
+          <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+            <FaHome /> Home
           </Link>
-        
-    </nav>
+        </li>
+
+        <li>
+          <Link to="/services">
+            <FaServicestack /> Services
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/about">
+            <FaInfoCircle /> About
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/clients">
+            <FaUsers /> Clients
+          </Link>
+        </li>
+
+        <li>
+          <button className="btn">BOOK NOW</button>
+        </li>
+      </ul>
+    </header>
   )
 }
 
